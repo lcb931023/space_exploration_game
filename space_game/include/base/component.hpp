f5b6a80f5f16e747b20b2ddd68f2755a2d3a1284
Grant <grant@tuxinator.net>
2014-06-23 21:58:54 -0400

    Trying to make a self managing component.

--- space_game/include/base/component.hpp
+++ space_game/include/base/component.hpp
@@ -1,12 +1,26 @@
 #ifndef COMPONENT_HPP
 #define COMPONENT_HPP
 
+#include <vector>
+#include <algorithm>
+
 namespace space {
     class component {
     public:
-        component (const int _type) : type (_type) {}
+        component (const int _type) : type (_type) {
+            comps.push_back (this);
+        }
+
+        ~component () {
+            std::remove (comps.begin (), comps.end (), this);
+        }
+
+        virtual void update (entity& _owner, game_context& _context) = 0;
 
         const int type;
+
+    public:
+        static std::vector<component*> comps;
     };
 }
 
