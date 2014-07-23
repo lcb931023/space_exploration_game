bf0d1fca57567a31bfc4dc9c74fca6a54779f36d
Grant <grant@tuxinator.net>
2014-07-23 18:48:31 -0400

    Finished basic controls and camera.

--- space_lander/src/main.cpp
+++ space_lander/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
+#include <cmath>
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
@@ -7,16 +8,22 @@
 
 const float FPS = 60;
 
-float shipX = 200, shipY = 200;
-float thrustX = 0, thrustY = 0;
-float velX = 0, velY = 0;
+struct entity {
+    float x, y;
+    float angle;
+
+    entity (float _x, float _y, float _a) : x (_x), y (_y), angle (_a) {}
+};
 
 struct world {
+    std::vector<entity> ents;
 };
 
 struct camera {
     int x, y;
     int w, h;
+
+    camera (int _x, int _y, int _w, int _h) : x (_x), y (_y), w (_w), h (_h) {}
 };
 
 int main (int argc, char** argv) {
@@ -58,6 +65,21 @@ int main (int argc, char** argv) {
     al_register_event_source (event_queue, al_get_timer_event_source (timer) );
     al_register_event_source (event_queue, al_get_keyboard_event_source () );
 
+    // init some crap for the game
+    float thrustX = 0, thrustY = 0, thrustR = 0;
+    float velX = 0, velY = 0, velR = 0;
+
+    entity thing1 (25, 50, 0);
+    entity thing2 (80, 80, 0);
+    entity thing3 (450, 400, 0);
+    entity ship (300, 300, 0);
+    world theWorld;
+    theWorld.ents.push_back (thing1);
+    theWorld.ents.push_back (thing2);
+    theWorld.ents.push_back (thing3);
+
+    camera cam (0, 0, 800, 600);
+
     // clear the screen and start the FPS timer
     al_clear_to_color (al_map_rgb (0, 0, 0) );
     al_flip_display ();
@@ -77,47 +99,91 @@ int main (int argc, char** argv) {
         }
         else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
             switch (ev.keyboard.keycode) {
-                case ALLEGRO_KEY_UP:
-                    thrustY = -0.02;
+                case ALLEGRO_KEY_W:
+                    thrustY = -0.01;
                     break;
-                case ALLEGRO_KEY_LEFT:
-                    thrustX = -0.02;
+                case ALLEGRO_KEY_S:
+                    thrustY = 0.01;
                     break;
-                case ALLEGRO_KEY_RIGHT:
-                    thrustX = 0.02;
+                case ALLEGRO_KEY_A:
+                    thrustX = -0.01;
+                    break;
+                case ALLEGRO_KEY_D:
+                    thrustX = 0.01;
+                    break;
+                case ALLEGRO_KEY_Q:
+                    thrustR = 0.00002;
+                    break;
+                case ALLEGRO_KEY_E:
+                    thrustR = -0.00002;
                     break;
             }
         }
         else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
             switch (ev.keyboard.keycode) {
-                case ALLEGRO_KEY_UP:
+                case ALLEGRO_KEY_W:
                     thrustY = 0;
                     break;
-                case ALLEGRO_KEY_LEFT:
+                case ALLEGRO_KEY_S:
+                    thrustY = 0;
+                    break;
+                case ALLEGRO_KEY_A:
                     thrustX = 0;
                     break;
-                case ALLEGRO_KEY_RIGHT:
+                case ALLEGRO_KEY_D:
                     thrustX = 0;
+                    break;
+                case ALLEGRO_KEY_Q:
+                    thrustR = 0;
+                    break;
+                case ALLEGRO_KEY_E:
+                    thrustR = 0;
                     break;
             }
         }
 
-        // do logic
+        // do logic for moving the ship
         velX += thrustX;
         velY += thrustY;
+        velR += thrustR;
 
-        if (velY < 1.0)
-            velY += 0.01;
-
-        shipX += velX;
-        shipY += velY;
+        ship.x += velX;
+        ship.y += velY;
+        ship.angle += velR;
 
         // redraw if there are no more events
         if (redraw && al_is_event_queue_empty (event_queue) ) {
             redraw = false;
             al_clear_to_color (al_map_rgb (0,0,0) );
 
-            al_draw_filled_circle (shipX, shipY, 20, al_map_rgb (255, 255, 255) );
+            // make the camera centered over the ship
+            cam.x = ship.x - (cam.w / 2);
+            cam.y = ship.y - (cam.h / 2);
+
+            for (entity e : theWorld.ents) {
+                // rotate the objects based on the camera's angle
+                float s = std::sin (ship.angle);
+                float c = std::cos (ship.angle);
+
+                float tempx = e.x;
+                float tempy = e.y;
+
+                tempx -= ship.x;
+                tempy -= ship.y;
+
+                float rotx = tempx * c - tempy * s;
+                float roty = tempx * s + tempy * c;
+
+                tempx = rotx + ship.x;
+                tempy = roty + ship.y;
+
+                // check if the entity is visible
+                if (tempx >= cam.x && tempy >= cam.y && tempx <= cam.x + cam.w && tempy <= cam.y + cam.h) {
+                    al_draw_filled_circle (tempx - cam.x, tempy - cam.y, 20, al_map_rgb (200, 0, 0) );
+                }
+            }
+
+            al_draw_filled_circle (ship.x - cam.x, ship.y - cam.y, 20, al_map_rgb (255, 255, 255) );
 
             al_flip_display ();
         }
